ee98a0b3098b3b8edd8cb93f1b396c7cff240d14
elsar <elsaryynanen@outlook.com>
2020-06-17 17:34:32 +0300

    deployment

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from flask import Flask,request, render_template
+from config import DevelopmentConfig 
 import psycopg2
 import psycopg2.extras 
 import csv
 import datetime
+
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+
 
 conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
 cur = conn.cursor()
@@ -10,13 +17,12 @@ cur = conn.cursor()
 ## CREATE TABLES
 def create_tables():  
     cur.execute(""" CREATE TABLE IF NOT EXISTS i_type(ufeffID integer, sequence text, pname text, pstyle text, description text, iconurl text, avatar integer, id serial PRIMARY KEY)""")
-    cur.execute(""" CREATE TABLE IF NOT EXISTS priority (ufeffID integer, sequence text, pname text, description text, iconurl text, statuscolor text, id serial PRIMARY KEY)""")
+    cur.execute(""" CREATE TABLE IF NOT EXISTS priority (ufeffID integer, sequence int, pname text, description text, iconurl text, statuscolor text, id serial PRIMARY KEY)""")
     cur.execute(""" CREATE TABLE IF NOT EXISTS i_status (ufeffID integer, sequence integer, pname text, description text, iconurl text, statuscategory text, id serial PRIMARY KEY)""")
     cur.execute(""" CREATE TABLE IF NOT EXISTS jira_i (ufeffID integer, pkey integer,project integer,reporter text, assignee text,issuetype integer, summary text,description text,environment text,priority integer, resolution integer, issuestatus integer, created bigint, updated bigint, duedate bigint, resolutiondate bigint,votes integer, watches integer, timeoriginalestimate integer, timeestimate integer,timespent integer,workflow_id integer,security integer,fixfor integer,component integer, issuenum integer,creator text,archived text, primary_key serial PRIMARY KEY) """)
     cur.execute(""" CREATE TABLE IF NOT EXISTS cgci (id_one integer, issueid integer, author text,created bigint, id_two integer,groupid integer,fieldtype text,field text,oldvalue text,oldstring text,newvalue text,newstring text, primary_id serial PRIMARY KEY)""")
-    print('tables done')
     conn.commit()
- 
+
 
 ## ISSUETYPE
 def insert_issuestype(): 
@@ -29,7 +35,6 @@ def insert_issuestype():
         "INSERT INTO i_type VALUES (%s, %s, %s, %s, %s, %s, %s)",
         row    
     )  
-    print('it done')
     conn.commit()
     
 
@@ -44,9 +49,8 @@ def insert_priority():
         "INSERT INTO priority VALUES (%s, %s, %s, %s, %s, %s)",
         row    
     ) 
-    print('pr done') 
     conn.commit()
- 
+
 ### ISSUESTATUS
 def insert_issuestatus(): 
  with open('./files/issuestatus.csv', 'r') as f:
@@ -55,10 +59,9 @@ def insert_issuestatus():
     for row in reader:
         row = [x if x !='NULL' else None for x in row]   
         cur.execute(
-        "INSERT INTO i_type VALUES (%s, %s, %s, %s, %s, %s)",
+        "INSERT INTO i_status VALUES (%s, %s, %s, %s, %s, %s)",
         row    
     )  
-    print('is done')
     conn.commit()
 
 ## JIRAISSUE
@@ -72,7 +75,7 @@ def insert_jiraissue():
         "INSERT INTO jira_i VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
         row    
     )
-    print('ji done') 
+
     conn.commit()
   
 
@@ -87,7 +90,7 @@ def insert_changegroup_changeitem():
         "INSERT INTO cgci VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
         row    
     )  
-    print('cgci done')   
+  
     conn.commit() 
 
 
@@ -159,10 +162,13 @@ def querydb(issuenumber,time_stamp):
         assignee = row[4]
         summary = row[6]
         description = row[7]
-        priority = row[9]
-        status = row[11]
+        pri = row[9]
+        priority = int(pri)
+        stat = row[11]
+        status = int(stat)
         created = row[12]
         closed =row[14]
+
 
     ### QUERY LOGS       
     db_query_logs = """select * 
@@ -175,37 +181,64 @@ def querydb(issuenumber,time_stamp):
 
     logs = cur.fetchall()
     if len(logs) == 0:
-        print('No issues with issuetype = "bug"')
+        print('No logs of issue')
+        exit
     
     updated = []
     for row in logs: 
         updated.append(row[3])
         
     conn.commit()
-    conn.close()
-   
-    return (created, closed,updated, summary, description, status, priority, assignee, reporter)
     
 
+    ### QUERY STATUS
+    db_query_issuestatus = """select sequence, pname
+                    from i_status 
+                    where sequence = %s ;    
+                    """
+
+    cur.execute(db_query_issuestatus, (status,)) 
     
+    res_status = cur.fetchone()
+    for row in res_status:
+        status_typed = row
+     
+        
+    conn.commit()
+
+
+    ## QUERY PRIORITY
+
+    db_query_priority = """select sequence, pname
+                    from priority 
+                    where sequence = %s ;    
+                    """
+
+    cur.execute(db_query_priority, (priority,)) 
+    
+    res_priority = cur.fetchone()
+    for row in res_priority:
+        priority = row
+          
+    conn.commit()
+
+    return (created, closed,updated, summary, description, status_typed, priority, assignee, reporter)
+    
+
 
 ## INPUTFUNCTION
-   
+
+@app.route('/')
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
 def get_input():
-    issue_number = int(input("Issue Number: "))
-    time = int(input("Time: "))
-    res = querydb(issue_number, time)
-    created, closed, updated, summary, description, status, priority, assignee, reporter = res
-    
-    print(f"Created:{created} ")
-    print(f"Closed:{closed} ")
-    print(f"Updated: {updated}")
-    print(f"Summary:{summary} ")
-    print(f"{description} ")
-    print(f"Status:{status}")
-    print(f"Priority: {priority}")
-    print(f"Assignee:{assignee}")
-    print(f"Reporter:{reporter}")   
+    issue_number = int(request.form['issueid'])
+    time_stamp = int(request.form['timestamp'])
+    res = querydb(issue_number, time_stamp)
+    created, closed, updated, summary, description, status, priority, assignee, reporter = res 
+    return render_template("result.html", issuenumber = issue_number, timestamp= time_stamp,created= created, closed=closed,updated=updated,summary=summary,description=description,status=status,priority=priority,assignee=assignee,reporter=reporter);   
     
 def source_input():
     create_tables()    
@@ -216,10 +249,13 @@ def source_input():
     insert_changegroup_changeitem() 
   
   
-
 def init():
     """ source_input() """
-    get_input() 
+    """ get_input()  """
 
-init()
+""" init() """
+
+if __name__ == '__main__':
+    app.run()
+
   
